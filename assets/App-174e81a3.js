c678e158997e0d7199076f8e215942b8ffb1cd7b
YAN-ZSY <115971667+m007m700@users.noreply.github.com>
2025-03-27 11:05:04 +0800

    Delete assets directory

--- assets/App-174e81a3.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as s}from"./index-6e03b95f.js";function r(){return t.jsx("div",{children:t.jsx(s,{})})}export{r as default};
--- assets/index-b1bb835f.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as e}from"./index-6e03b95f.js";function r(){return t.jsx("div",{children:t.jsx(e,{})})}export{r as default};
