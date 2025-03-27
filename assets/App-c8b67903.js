eb65e68f533abd7c5b9abd1f6b9ebf9f326a5128
YAN-ZSY <115971667+m007m700@users.noreply.github.com>
2025-03-27 11:06:45 +0800

    Add files via upload

---
+++ assets/App-c8b67903.js
@@ -0,0 +1,1 @@
+import{j as t,O as s}from"./index-f0ff529e.js";function r(){return t.jsx("div",{children:t.jsx(s,{})})}export{r as default};
---
+++ assets/index-99314fd7.js
@@ -0,0 +1,1 @@
+import{j as t,O as e}from"./index-f0ff529e.js";function r(){return t.jsx("div",{children:t.jsx(e,{})})}export{r as default};
